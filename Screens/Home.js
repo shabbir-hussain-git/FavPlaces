d40910844ffcf485cdda1a639da8c3b596414562
Shabbir Hussain <pshabbir36@gmail.com>
2022-11-05 18:43:31 +0530

    List Added

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,16 +1,31 @@
-import { Text } from "react-native"
-import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { FlatList, Text ,StyleSheet,View} from "react-native"
+import { useSelector } from "react-redux";
+import FavItem from "../components/FavItem";
+import COLORS from "../Util/Colors";
 
 const Home = ()=>{
 
-    const myIcon = <Icon name="rocket" size={30} color="#900" />;
-
+    const state = useSelector(store=>store.fav);
+    console.log(state.favPlaces)
+    const getFavPlace = ({item})=>{
+        return <FavItem item={item}/>
+    }
     return (
-        <>
-            <Text>Home</Text>
-            {myIcon}
-        </>
-    )   
+      <>
+        <View style={styles.container}>
+          <FlatList
+            data={state.favPlaces}
+            keyExtractor={item => item.id}
+            renderItem={getFavPlace}
+          />
+        </View>
+      </>
+    );   
 }
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:COLORS.primary_300
+    }
+})
 export default Home;
