2a7b353b5b89df11f798cec412c304111c29ecaf
Shabbir Hussain <pshabbir36@gmail.com>
2022-11-07 20:02:37 +0530

    bug fixed

--- components/MapViewScreen.js
+++ components/MapViewScreen.js
@@ -1,6 +1,6 @@
 import {View,StyleSheet } from 'react-native'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-
+//
 const MapViewScreen = ()=>{
 
     const onUserPressed = (data)=>{
